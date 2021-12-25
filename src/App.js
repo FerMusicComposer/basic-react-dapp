764bc1ad7bf3a596002ca3f80def76182528b26b
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2021-12-25 16:29:32 +0100

    front for interaction with greeter SC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 import './App.css';
 
+const greeterAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [greetingValue, setGreetingValue] = useState('');
+
+    const requestAccount = async () => {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+    };
+
+    const fetchGreeting = async () => {
+        if (typeof window.ethereum !== 'undefined') {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+            try {
+                const data = await contract.greet();
+                console.log('data: ', data);
+            } catch (err) {
+                console.log('Error: ', err);
+            }
+        }
+    };
+
+    const setGreeting = async () => {
+        if (!greetingValue) return;
+        if (typeof window.ethereum !== 'undefined') {
+            await requestAccount();
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+            const transaction = await contract.setGreeting(greetingValue);
+            setGreetingValue('');
+            await transaction.wait();
+            fetchGreeting();
+        }
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={fetchGreeting}>Fetch Greeting</button>
+                <button onClick={setGreeting}>Set Greeting</button>
+
+                <input
+                    onChange={e => setGreetingValue(e.target.value)}
+                    placeholder="Set greeting"
+                    value={greetingValue}
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
